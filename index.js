d94902d95c0d60ef636220ca57c207dd78473d1a
ashachakre0906 <chourpagar.asha@gmail.com>
2023-06-05 23:06:22 -0700

    refactored the function caldisplaysummary which returns incomes, outcomes and interest values

--- index.js
+++ index.js
@@ -123,13 +123,21 @@ const calcDisplaySummary = function (movements) {
     const incomes = movements.filter(mov => mov > 0).reduce((acc, mov) => acc + mov, 0);
     console.log(incomes);
     labelSumIn.textContent = `${incomes}€`;
+
+      const out = movements
+        .filter((mov) => mov < 0)
+        .reduce((acc, mov) => acc + mov, 0);
+      console.log(out);
+    labelSumOut.textContent = `${Math.abs(out)}€`;
+    
+    const interest = movements.filter(mov => mov > 0).map(deposit =>
+        (deposit * 1.2) / 100).filter((int, i, arr) => {
+            console.log(arr);
+            return int >= 1 //filtered out the interst greater than 1
+        }).reduce((acc, int) => acc + int, 0);
+    labelSumInterest.textContent = `${interest}€`
+   console.log(interest);
 }
 calcDisplaySummary(account1.movements);
 
-//Display the total outcome
-const outcome = function (movements) {
-    const out = movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0);
-    console.log(out);
-    labelSumOut.textContent = `${Math.abs(out)}€`
-}
-outcome(account1.movements);
+
